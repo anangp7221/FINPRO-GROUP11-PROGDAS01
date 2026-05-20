6ac69dfa8c6ef2d372982e2f42caa811914bece9
anangp7221 <143253237+anangp7221@users.noreply.github.com>
2026-05-20 23:52:31 +0700

    Initial: Setup project structure and constants

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define BATAS_SUHU_MAKS 33.0        
+#define BATAS_KELEMBAPAN_MIN 70.0   
+#define JUMLAH_BLOK 5
+
+int main() {
+    return 0;
+}
